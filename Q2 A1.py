4acbb5ae432a5e7dc3989b008c43b889c06c7f56
kislaykumar1210 <120155915+kislaykumar1210@users.noreply.github.com>
2022-12-22 16:46:23 +0530

    Update Q2 A1.py

--- Q2 A1.py
+++ Q2 A1.py
@@ -1,32 +1,7 @@
-def calculate(amount, percent):
-    return (amount * percent) / 100
+GrossIncome = int(input("Enter Your Gross Income  "))
+Dependents = int( input("Enter the number of Dependemts-  "))
 
+Tax_1 = GrossIncome-10000-(Dependents*3000)
+Final_Tax = Tax_1*0.2
 
-def calculate_income_tax(total_income:
-float) -> float:
-    if total_income <= 250000:
-        return 5000
-    elif total_income <= 500000:
-        return calculate(total_income -
-                         250000, 5000)
-    elif total_income <= 750000:
-        return calculate(total_income -
-                         500000, 20)
-    elif total_income <= 1000000:
-        return calculate(total_income -
-                         750000, 20) - 10000
-    elif total_income <= 1250000:
-        return calculate(total_income -
-                         1000000, 20) - 10000
-    elif total_income <= 1500000:
-        return calculate(total_income -
-                         1250000, 20) - 10000
-    else:
-        return calculate(total_income -
-                         1500000, 20) - 10000
-
-
-if __name__ == '__main__':
-    total_income = float(input("What's your annual income?\n>>> "))
-    tax = calculate_income_tax(total_income)
-    print(f"Total tax applicable at ₹{total_income} is ${tax}")
+print("Your tax is $ ",Final_Tax)
