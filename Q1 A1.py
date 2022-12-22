748d35fb610f0d61e1f94c6a2a48375fa26ce55f
kislaykumar1210 <120155915+kislaykumar1210@users.noreply.github.com>
2022-12-22 16:28:42 +0530

    Add files via upload

---
+++ Q1 A1.py
@@ -0,0 +1,11 @@
+# Question1
+
+a = int (input(" Please enter the first number: "))
+b = int (input(" Please enter the second number: "))
+c = int (input(" please enter the third number: "))
+average = (a+b+c)/3
+
+print(" The average of three numbers is ", average)
+
+
+
